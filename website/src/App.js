f46b2b80a0ecb04e0bc54b79b1217be8fb130a11
Trevor Richardson <Tjrich336@gmail.com>
2023-05-18 15:59:45 -0400

    Added 'About' & 'Header' pages to App.js.

--- website/src/App.js
+++ website/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
-
+import Header from './components/Header/Header';
 import Landing from './components/Landing/Landing';
+import About from './components/About/About';
 
 const App = () => {
   return (
     <>
+    < Header />
 
     <main className='main'>
       <Landing />
+      <About />
     </main>
     
     </>
