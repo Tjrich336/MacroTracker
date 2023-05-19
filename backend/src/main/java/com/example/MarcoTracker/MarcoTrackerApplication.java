db0f85b1c1e4a2a96575d1585bfb8c7b1b6b9c81
brandonhach <specialcambo@gmail.com>
2023-05-19 12:05:16 -0400

    Initial Commit

---
+++ backend/src/main/java/com/example/MarcoTracker/MarcoTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.MarcoTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcoTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcoTrackerApplication.class, args);
+	}
+
+}
