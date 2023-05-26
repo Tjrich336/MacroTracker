55865b10308f5845e3a2fa229c32f6154e1a9361
brandonhach <specialcambo@gmail.com>
2023-05-26 10:58:07 -0400

    added Controller

---
+++ marcoTrackerEnd/src/main/java/com/example/marcoTrackerEnd/MarcoTrackerController.java
@@ -0,0 +1,9 @@
+package com.example.marcoTrackerEnd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MarcoTrackerController {
+
+}
