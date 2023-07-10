d3c403e68ada7a567507a331a25b35febb97683e
brandonhach <specialcambo@gmail.com>
2023-07-10 00:58:02 -0400

    Redone landing page 1/2

--- website/src/components/Landing/Landing.jsx
+++ website/src/components/Landing/Landing.jsx
@@ -1,32 +1,24 @@
 import React from 'react';
 import './landing.css';
+import futuristicHealth from '../../assets/futuristicHealth.png';
+import LandingTextBlock from "./TextBlock/LandingTextBlock";
+import ThreeScene from '../Three/ThreeScene';
 
 const Landing = () => {
   const handleButtonClick = () => {
     window.location.href = '/signup';
   };
+  
 
   return (
     <section className="landing section" id="landing">
-      <div className="landing__container container grid">
-        <div className="landing__content grid">
-          <div className="landing__img"></div>
-          <h2 className="landing__message">
-            Discover a healthier you with the help of a Macro Tracking website! Take
-            charge of your well-being by effortlessly monitoring your daily vitamin
-            intake and achieving optimal nutrition. With this user-friendly platform,
-            you can easily log and track the vitamins you consume, ensuring you meet
-            your recommended daily allowances. Get valuable insights into your nutrient
-            profile, set personalized goals, and receive tailored recommendations to
-            fill any gaps in your diet. Whether you're striving for improved energy
-            levels, a stronger immune system, or overall wellness, this website
-            empowers you to make informed choices and cultivate a balanced lifestyle.
-            Start your journey towards a vibrant and nourished self today!
-          </h2>
+      <div className="container">
+        <div className="container_img"><img src={futuristicHealth} alt="" /></div>
+        <div className="container_text">
+          <h1>Discover a healthier you!</h1>
+          <LandingTextBlock className="landingTextBlock"></LandingTextBlock>
+          <button className="login-button" onClick={handleButtonClick}> Get Started Today! </button>
         </div>
-        <button className="login-button" onClick={handleButtonClick}>
-            Get Started Today!
-        </button>
       </div>
     </section>
   );
---
+++ website/src/components/Landing/TextBlock/LandingTextBlock.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const LandingTextBlock = ({className}) => {
+  return (
+    <h2 className={className}> Take
+    charge of your well-being by effortlessly monitoring your daily marco
+    intake and achieving optimal nutrition. With this user-friendly platform,
+    you can easily log and track the vitamins you consume, ensuring you meet
+    your recommended daily allowances. Get valuable insights into your nutrient
+    profile, set personalized goals, and receive tailored recommendations to
+    fill any gaps in your diet. Whether you're striving for improved energy
+    levels, a stronger immune system, or overall wellness, this website
+    empowers you to make informed choices and cultivate a balanced lifestyle.
+    Start your journey towards a vibrant and nourished self today!</h2>
+  )
+}
+
+export default LandingTextBlock
